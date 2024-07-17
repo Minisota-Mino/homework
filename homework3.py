eed4adbfd84356b533381a4f042c9d9c6fd9b5cb
Владимир <vovanovoselop@gmail.com>
2024-07-17 23:40:51 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,15 @@
+name = str('Vladimir')
+age = int(16)
+new_age = 2 + int(age)
+is_student = True
+
+print('Name:', name)
+print('Age:', age)
+print('New age:', new_age)
+print('Is_student:', is_student)
+
+
+
+
+
+
