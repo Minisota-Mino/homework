dba89c0a5e86f4debb6624eb77793ba6d34dafd0
Minisota-Mino <vovanovoselop@gmail.com>
2024-07-17 23:10:02 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+course_name = 'Python,'
+completed_tasks = 12
+hour_spent = 1.5
+hours_per_task = hour_spent / completed_tasks
+print('Курс:', course_name, 'всего задач:', completed_tasks, "затрачено часов:", hour_spent, 'среднее выполнение одной задачи:', hours_per_task, 'часа')
+    
